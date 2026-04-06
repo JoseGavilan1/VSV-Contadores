e90503894e6a6935f581c3041053ea2b24ff5bf1
Felipe <felipe.veram2001@gmail.com>
2026-04-06 15:15:30 -0400

    prueba 1.2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const IP_PUBLICA = 'localhost';
 
-export const API_BASE_URL = "https://vsv-contadores-1.onrender.com";
+export const API_BASE_URL = "https://vsv-contadores-1.onrender.com/api";
 export const FRONTEND_URL = `http://${IP_PUBLICA}:3000`;
 export const RUT_EMPRESA_LOCAL = '78306207-0';
 export const IVA_RATE = 0.19;
