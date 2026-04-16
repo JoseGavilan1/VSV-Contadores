fdeee77bfa6cb5ae340754a73ddd6d52ca1ba719
Felipe <felipe.veram2001@gmail.com>
2026-04-16 16:59:17 -0400

    1.3

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const IP_PUBLICA = 'https://bcfckukvgojnfmmwoqpf.supabase.co';
 
-export const API_BASE_URL = `http://${IP_PUBLICA}`;
+export const API_BASE_URL = `${IP_PUBLICA}`;
 export const FRONTEND_URL = `http://${IP_PUBLICA}:3000`;
 export const RUT_EMPRESA_LOCAL = '78306207-0';
 export const IVA_RATE = 0.19;
